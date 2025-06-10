9ac32c8d0f87a7c18273b108bca21db03702c5ff
Matthew Chadwick <dr.m.chadwick@gmail.com>
2025-06-10 22:19:45 +0100

    Added recipe to fix the noconsole behaviour on pyinstaller projects

--- pubreplaygui/replay.py
+++ pubreplaygui/replay.py
@@ -4,6 +4,7 @@ import subprocess
 from subprocess import CompletedProcess
 from queue import Queue
 from concurrent.futures import ProcessPoolExecutor
+import os
 
 
 def get_replay_files(replay_path: Path, min_bytes: int = 1000000) -> set[Path]:
@@ -22,6 +23,57 @@ def get_replay_files(replay_path: Path, min_bytes: int = 1000000) -> set[Path]:
 def get_json_files(output_path: Path) -> set[Path]:
     return set(Path(output_path).glob('*.json'))
 
+# https://github.com/pyinstaller/pyinstaller/wiki/Recipe-subprocess PyInstaller and popen+tkinter
+# Create a set of arguments which make a ``subprocess.Popen`` (and
+# variants) call work with or without Pyinstaller, ``--noconsole`` or
+# not, on Windows and Linux. Typical use::
+#
+#   subprocess.call(['program_to_run', 'arg_1'], **subprocess_args())
+#
+# When calling ``check_output``::
+#
+#   subprocess.check_output(['program_to_run', 'arg_1'],
+#                           **subprocess_args(False))
+def subprocess_args(include_stdout=True):
+    # The following is true only on Windows.
+    if hasattr(subprocess, 'STARTUPINFO'):
+        # On Windows, subprocess calls will pop up a command window by default
+        # when run from Pyinstaller with the ``--noconsole`` option. Avoid this
+        # distraction.
+        si = subprocess.STARTUPINFO()
+        si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+        # Windows doesn't search the path by default. Pass it an environment so
+        # it will.
+        env = os.environ
+    else:
+        si = None
+        env = None
+
+    # ``subprocess.check_output`` doesn't allow specifying ``stdout``::
+    #
+    #   Traceback (most recent call last):
+    #     File "test_subprocess.py", line 58, in <module>
+    #       **subprocess_args(stdout=None))
+    #     File "C:\Python27\lib\subprocess.py", line 567, in check_output
+    #       raise ValueError('stdout argument not allowed, it will be overridden.')
+    #   ValueError: stdout argument not allowed, it will be overridden.
+    #
+    # So, add it only if it's needed.
+    if include_stdout:
+        ret = {'stdout': subprocess.PIPE}
+    else:
+        ret = {}
+
+    # On Windows, running this from the binary produced by Pyinstaller
+    # with the ``--noconsole`` option requires redirecting everything
+    # (stdin, stdout, stderr) to avoid an OSError exception
+    # "[Error 6] the handle is invalid."
+    ret.update({'stdin': subprocess.PIPE,
+                'stderr': subprocess.PIPE,
+                'startupinfo': si,
+                'env': env })
+    return ret
+
 
 def process_replay(replay_in: Path, json_out: Path, ignore_existing=False) -> CompletedProcess:
     if ignore_existing and json_out.exists():
@@ -35,7 +87,8 @@ def process_replay(replay_in: Path, json_out: Path, ignore_existing=False) -> Co
         str(json_out)
         ]
 
-    return subprocess.run(process_args, capture_output=True, text=True)
+    # return subprocess.run(process_args, capture_output=True, text=True, **subprocess_args(include_stdout=True))
+    return subprocess.run(process_args, text=True, **subprocess_args(include_stdout=True))
 
 
 def process_replay_path(
