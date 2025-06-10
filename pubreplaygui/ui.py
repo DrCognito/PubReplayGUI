ec9ac6d7f08a287dffde7392ea26749b483a5963
Matthew Chadwick <dr.m.chadwick@gmail.com>
2025-06-10 21:04:52 +0100

    Added more entry update events. Added padding to various objects and frames. Changed logwidget init to specify level and not remove all other loggers. Added replay queue and handler for threaded events. Improved replay_path setter implementation with process as well. Added function to process replay_queue events. Changed replay process to use the threaded implementation.

--- pubreplaygui/ui.py
+++ pubreplaygui/ui.py
@@ -4,10 +4,10 @@ from tkinter import filedialog
 from pathlib import Path
 import os as os
 from pubreplaygui import LOG
-from pubreplaygui.replay import process_replay_path
+from pubreplaygui.replay import get_replay_files, process_replay, thread_replays
 from tkloguru import LoguruWidget, setup_logger
-import subprocess
-
+from queue import Queue, Empty
+from concurrent.futures import Future
 
 class MainApplication(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
@@ -17,6 +17,8 @@ class MainApplication(tk.Frame):
         self.loc_picker = ttk.Frame()
         self.entry = ttk.Entry(master=self.loc_picker, width=100)
         self.entry.pack(side=tk.LEFT, expand=True)
+        self.entry.bind('<FocusOut>', self._update_entry)
+        self.entry.bind('<Return>', self._update_entry)
 
         self.browse_button = ttk.Button(
             master=self.loc_picker,
@@ -24,7 +26,7 @@ class MainApplication(tk.Frame):
             command=self.directory_picker
         )
         self.browse_button.pack(side=tk.RIGHT)
-        self.loc_picker.pack()
+        self.loc_picker.pack(pady=5, padx=5)
 
         self.doer_frame = ttk.Frame()
         self.process_button = ttk.Button(
@@ -45,19 +47,29 @@ class MainApplication(tk.Frame):
             text="Show replays",
         )
         self.rep_button.pack(side=tk.RIGHT, expand=True, fill='x')
-        self.doer_frame.pack()
+        self.doer_frame.pack(pady=5, padx=5)
         
         # Logging frame
         # Intercept logging is off by default
         self.log_widget = LoguruWidget(self.parent, show_scrollbar=True, color_mode='level', max_lines=1000)
-        self.log_widget.pack(expand=True, fill=tk.BOTH, padx=10, pady=10)
+        self.log_widget.pack(expand=True, fill=tk.BOTH, padx=5, pady=5)
 
-        setup_logger(self.log_widget)
+        # Do this manually to not remove current handler
+        # setup_logger(self.log_widget)
+        LOG.add(self.log_widget.sink, backtrace=True, diagnose=True, level="INFO")
+        
+        # Progress bar
+        self.progress_bar = ttk.Progressbar(orient=tk.HORIZONTAL, length=100,)
+        self.progress_bar.pack(fill=tk.X, padx=5, pady=5)
         
         self.output_path: Path | None = None
         self._replay_path: Path | str = ''
         self._save_config: function | None = None
         self._config = None
+        # Replay processing queue
+        self.replay_queue = Queue()
+        # Register the replay queue check
+        self.parent.after(200, self.check_queue)
 
     @property
     def replay_path(self) -> Path | str:
@@ -67,6 +79,7 @@ class MainApplication(tk.Frame):
     def replay_path(self, rpath: Path | str):
         if type(rpath) is str:
             rpath = Path(rpath)
+        self._replay_path = rpath
         valid = MainApplication.validate_directory(rpath)
         if valid:
              # Save a valid path to the config.ini
@@ -75,12 +88,14 @@ class MainApplication(tk.Frame):
                 self._save_config()
 
             show_replays = lambda: os.startfile(rpath)
-            self.rep_button.config(command= show_replays)
+            self.rep_button.config(command=show_replays)
             self.rep_button.config(state=tk.NORMAL)
+            self.process_button.config(state=tk.NORMAL)
         else:
             self.entry.config({"background": "red"})
             self.rep_button.config(command=None)
             self.rep_button.config(state=tk.DISABLED)
+            self.process_button.config(state=tk.DISABLED)
     
     def directory_picker(self):
         directory = filedialog.askdirectory(mustexist=True)
@@ -89,6 +104,9 @@ class MainApplication(tk.Frame):
         self.replay_path = directory
 
         return
+    
+    def _update_entry(self, event):
+        self.replay_path = self.entry.get()
 
     @staticmethod
     def validate_directory(directory: str | Path) -> bool:
@@ -108,10 +126,42 @@ class MainApplication(tk.Frame):
         LOG.debug(f"Directory {dir_path} is valid.")
         return True
     
-    def process_replays(self, reprocess=True):
-        replay_dir = Path(self.entry.get())
-        process_replay_path(replay_dir, self.output_path, reprocess)
+    # def process_replays(self, reprocess=True):
+    #     replay_dir = Path(self.entry.get())
+    #     process_replay_path(replay_dir, self.output_path, reprocess, self.progress_bar)
         
+    #     return
+    def check_queue(self):
+        running = Queue()
+        while self.replay_queue.qsize():
+            try:
+                replay:str
+                task: Future
+                replay, task = self.replay_queue.get_nowait()
+                if not task.done():
+                    running.put((replay, task))
+                    continue
+                # Expect dict[name:task]
+                LOG.info(f"Processed {replay}")
+                LOG.debug(f"Process for {replay} completed with:")
+                LOG.debug(task.result().stdout)
+                self.progress_bar.step()
+            except Empty:
+                LOG.debug("All ")
+                pass
+        # Re-add running
+        self.replay_queue = running
+        # Re-register our check
+        self.parent.after(20, self.check_queue)
+
+
+    def process_replays(
+        self, reprocess=True):
+        thread_replays(
+            self.replay_path, self.output_path,
+            reprocess, self.progress_bar, self.replay_queue)
+        LOG.debug("Submitted all replays.")
+
         return
 if __name__ == "__main__":
     root = tk.Tk()
