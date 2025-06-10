348cd07ed2246b8b25d29f7bff1de636cf94e8d8
Matthew Chadwick <dr.m.chadwick@gmail.com>
2025-06-10 21:00:28 +0100

    Implemented basic function to check the environment on load. Added window title. Better init for replay path.

--- pubreplaygui/main.py
+++ pubreplaygui/main.py
@@ -6,6 +6,23 @@ from pubreplaygui.replay import process_replay_path
 import tkinter as tk
 import tkinter.ttk as ttk
 
+
+def environment_check():
+    valid = True
+    if not CONFIG_LOC.exists():
+        LOG.error("Config.ini is missing.")
+        valid = False
+    if not PARSER_LOC.exists():
+        LOG.error(f"Parser binary is missing at {PARSER_LOC}")
+        valid = False
+    if not Path("./output").exists():
+        LOG.error("Output folder is missing.")
+        valid = False
+    if valid:
+        LOG.info("Environment check complete without error.")
+    else:
+        LOG.error("Environment check failed!")
+
 if __name__ == "__main__":
     # Setup the gui
     root = tk.Tk()
@@ -14,11 +31,16 @@ if __name__ == "__main__":
     # Save config lambda
     config = get_create_config()
     LOG.debug("Loaded config file.")
+    # Set the directory to our saved directory
     main_ui.entry.insert(0, config['PATHS']['replays'])
+    main_ui.replay_path = config['PATHS']['replays']
     # Output
     main_ui.output_path = Path(config["PATHS"]['output']).resolve()
     main_ui._config = config
     main_ui._save_config = lambda : save_config(CONFIG_LOC, config)
-    # Set the directory to our saved directory
+    # Env check
+    environment_check()
+    # Start the UI
     LOG.debug("Starting UI")
+    root.title("PubReplay GUI")
     root.mainloop()
