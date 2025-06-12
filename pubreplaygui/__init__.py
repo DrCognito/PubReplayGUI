aa315eefbea1be11c45fcf4d11969d459fff7d78
Matthew Chadwick <dr.m.chadwick@gmail.com>
2025-06-12 17:03:46 +0100

    Update for new parser name

--- pubreplaygui/__init__.py
+++ pubreplaygui/__init__.py
@@ -5,4 +5,4 @@ LOG.remove()
 LOG.add("app.log", rotation="5 MB", level='DEBUG', retention=0)
 
 CONFIG_LOC = Path("config.ini")
-PARSER_LOC = Path("./bin/app.exe")
+PARSER_LOC = Path("./bin/PubParser.exe")
